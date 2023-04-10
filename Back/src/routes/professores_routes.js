6e60d28ba8838a1eb8f1a35a31aaa18167821789
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2023-04-10 11:11:41 -0300

    add

---
+++ Back/src/routes/professores_routes.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+
+const router = express.Router();
+
+const Middleware = require("../middleware/middleware");
+
+const professor = require("../controllers/professores_controller");
+
+router.get("/read", professor.read);
+router.delete("/delete/:id_prof", professor.excluir);
+router.post("/login", professor.login);
+router.post("/create", professor.create);
+router.put("/update/:id_prof", professor.update);
+
+module.exports = router;
