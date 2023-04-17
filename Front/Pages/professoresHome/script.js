0321028f343073ae4a3d132eb0c88683532b0406
RodrickCS <caetanorodrigo46@gmail.com>
2023-04-17 10:54:09 -0300

    update

--- Front/Pages/professoresHome/script.js
+++ Front/Pages/professoresHome/script.js
@@ -2,7 +2,46 @@ const uriReadOneProf = "http://localhost:3000/professores/readOne/";
 const uriCreateTurma = "http://localhost:3000/turmas/create";
 const uriAddProf = "http://localhost:3000/turmas/adicionarProfessor/";
 
+var dadosCard = [];
+
 var professor = [];
+
+const openModal = () => {
+  document.querySelector(".back_modal").classList.remove("model");
+};
+const closeModal = () => {
+  document.querySelector(".back_modal").classList.add("model");
+};
+
+const checkUser = () => {
+  if (localStorage.getItem("token") !== null) {
+    const tokenJWT = localStorage.getItem("token");
+
+    try {
+      const payload = JSON.parse(atob(tokenJWT.split(".")[1]));
+      const expiracao = payload.exp;
+      const agora = Math.floor(Date.now() / 1000);
+
+      if (agora >= expiracao) {
+        logout();
+        return false;
+      }
+
+      return true;
+    } catch (err) {
+      logout();
+      return false;
+    }
+  } else {
+    window.location.href = "../login/index.html";
+  }
+};
+
+const logout = () => {
+  window.location.href = "../login/index.html"
+  localStorage.removeItem("token")
+  localStorage.removeItem("info_user_login")
+}
 
 const fetchOneProfessor = () => {
   let info = localStorage.getItem("info_user_login");
@@ -19,13 +58,16 @@ const fetchOneProfessor = () => {
     })
     .then((data) => {
       professor = data;
-      console.log(professor);
+      buildTurmasCard(data.turma);
     });
 };
 
 const criarTurma = () => {
+  checkUser();
+  let inputNome = document.querySelector("#nome_turma");
+
   let form = {
-    nome: "Turma de Desenvolvimento de Sistemas 3DES",
+    nome: inputNome.value,
   };
 
   const options = {
@@ -45,7 +87,7 @@ const criarTurma = () => {
       if (data.id_turma) {
         adicionarProfessor(data.id_turma, professor.id_prof);
       } else {
-        alert("Hove um erro na hora de criar uma turma");
+        alert("Hove um erro na criação uma turma");
       }
     });
 };
@@ -71,11 +113,60 @@ const adicionarProfessor = (id_turma, id_prof) => {
       console.log(data);
     });
 };
-
 const buildTurmasCard = (dados) => {
-  const divPai = document.createElement("div");
-  const divHeader = document.createElement("div");
-  const divBody = document.createElement("div");
+  dados.forEach((elemento) => {
+    const divPai = document.createElement("div");
+    const divHeader = document.createElement("div");
+    const imgHeader = document.createElement("img");
+    const divBody = document.createElement("div");
+
+    const h1 = document.createElement("h1");
+
+    divHeader.appendChild(imgHeader);
+    divPai.appendChild(divHeader);
+    divPai.appendChild(divBody);
+    divBody.appendChild(h1);
+
+    divPai.classList.add("turma_card");
+    divHeader.classList.add("card_header");
+    divBody.classList.add("card_body");
+    imgHeader.classList.add("card_img");
+
+    imgHeader.src = "../../Assets/icone.png";
+    imgHeader.style.width = "100px";
+
+    divPai.style.width = "24%";
+    divPai.style.backgroundColor = "#FFFFFF";
+    divPai.style.height = "170px";
+    divPai.style.display = "flex";
+    divPai.style.flexDirection = "column";
+    divPai.style.cursor = "pointer";
+    divPai.style.alignItems = "center";
+    divPai.style.borderRadius = "12px";
+    divPai.style.marginTop = "12px";
+
+    divBody.style.width = "100%";
+    divBody.style.height = "230px";
+    divBody.style.display = "flex";
+    divBody.style.alignItems = "center";
+    divBody.style.justifyContent = "center";
+    divBody.style.backgroundColor = "#19dde0";
+    divBody.style.borderRadius = "12px";
+    divBody.style.padding = "12px";
+
+    divHeader.style.height = "100px";
+    h1.innerHTML = elemento.nome;
+
+    divPai.addEventListener("click", function () {
+      window.location.href = "../turmasHome/index.html";
+    });
+
+    document.querySelector(".content").appendChild(divPai);
+  });
 };
 
 fetchOneProfessor();
+checkUser();
+setInterval(() => {
+  checkUser();
+}, 10000);
