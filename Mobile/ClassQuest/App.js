c210eb23f6d45d03ddb6ac592a389082b43137bb
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2023-05-16 18:40:49 -0300

    add

--- Mobile/ClassQuest/App.js
+++ Mobile/ClassQuest/App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import Menu from './src/pages/menu/menu';
 import Login from './src/pages/login/login';
 import Home from './src/pages/home/home';
 import Professor from './src/pages/professor/professor';
@@ -10,14 +11,14 @@ import Cadastro from './src/pages/cadastro/cadastro';
 const Stack = createNativeStackNavigator();
 export default function App() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Login" component={Login} />
-                {/* <Stack.Screen name="Home" component={Home} /> */}
-                <Stack.Screen name="Cadastro" component={Cadastro} />
+        // <NavigationContainer>
+            {/* <Stack.Navigator screenOptions={{ headerShown: false }}> */}
+                {/* <Stack.Screen name="Login" component={Login} /> */}
+                <Stack.Screen name="Home" component={Home} /> 
+                {/* <Stack.Screen name="Cadastro" component={Cadastro} /> */}
                 {/* <Stack.Screen name="Professor" component={Professor} /> 
                 <Stack.Screen name="Aluno" component={Aluno} /> */}
-            </Stack.Navigator>
-        </NavigationContainer>
+            // </Stack.Navigator>
+        // </NavigationContainer>
     );
 }
