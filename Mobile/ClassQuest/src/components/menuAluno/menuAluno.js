9039aadbfdd25f761f09a29c9328e9f1e3c52dbd
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2023-05-18 10:55:58 -0300

    add

--- Mobile/ClassQuest/src/components/menuAluno/menuAluno.js
+++ Mobile/ClassQuest/src/components/menuAluno/menuAluno.js
@@ -13,7 +13,7 @@ export default function Menu({ navigation }) {
 
     return (
         <Drawer.Navigator screenOptions={{ headerShown: false }} 
-        // initialRouteName='Home'
+        initialRouteName='Home'
         >
             <Drawer.Screen name="Perfil" component={Perfil}
                 options={{
