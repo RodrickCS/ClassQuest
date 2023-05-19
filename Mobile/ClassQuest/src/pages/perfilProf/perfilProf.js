82b852bae2557300cba7f3d2f97fe4308e5d8624
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2023-05-19 10:52:47 -0300

    add

--- Mobile/ClassQuest/src/pages/perfilProf/perfilProf.js
+++ Mobile/ClassQuest/src/pages/perfilProf/perfilProf.js
@@ -1,56 +1,53 @@
 import { View, Text, TouchableOpacity, Image, ImageBackground } from 'react-native';
-import { TextInput } from "react-native-paper";
+import { useState, useEffect } from "react";
 import styles from './style'
 
 export default function Perfil({ navigation }) {
 
-    // const [id_aluno, setId_aluno] = useState('');
+    const [turma, setTurma] = useState('');
 
     const menu = () => {
         navigation.openDrawer();
     }
 
     var user = JSON.parse(localStorage.getItem('nome'))
-    var id_alu = user.id_aluno
-    // var id_aluno = JSON.parse(localStorage.getItem('id_aluno'))
+    var id_prof = user.id_prof
 
-    console.log(user)
+    function info() {
+        let form = {
+            email: Email,
+            senha: Senha,
+        };
 
-    // function info() {
-    //     let form = {
-    //         email: Email,
-    //         senha: Senha,
-    //     };
+        let uri = `http://localhost:3000/readOne/${id_prof}`;
 
-    //     let uri = `http://localhost:3000/readOne/${id_aluno}`;
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+        };
 
-    //     const options = {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //         body: JSON.stringify(form),
-    //     };
-
-    //     fetch(uri, options)
-    //         .then((resp) => {
-    //             if (resp.ok) {
-    //                 return resp.json();
-    //             } else if (resp.status === 401) {
-    //                 throw new Error("Senha incorreta");
-    //             } else if (resp.status === 404) {
-    //                 throw new Error("Usuário não encontrado");
-    //             } else {
-    //                 throw new Error("Erro interno do servidor");
-    //             }
-    //         })
-    //         .then((data) => {
-    //             console.log(data);
-    //         })
-    //         .catch((error) => {
-    //             console.error(error);
-    //         });
-    // }
+        fetch(uri, options)
+            .then((resp) => {
+                if (resp.ok) {
+                    return resp.json();
+                } else if (resp.status === 401) {
+                    throw new Error("Senha incorreta");
+                } else if (resp.status === 404) {
+                    throw new Error("Usuário não encontrado");
+                } else {
+                    throw new Error("Erro interno do servidor");
+                }
+            })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
 
     // function update() {
     //     let form = {
@@ -58,7 +55,7 @@ export default function Perfil({ navigation }) {
     //         senha: Senha,
     //     };
 
-    //     let uri = `http://localhost:3000/readOne/${id_aluno}`;
+    //     let uri = `http://localhost:3000/readOne/${id_prof}`;
 
     //     const options = {
     //         method: "POST",
@@ -94,7 +91,7 @@ export default function Perfil({ navigation }) {
     //         senha: Senha,
     //     };
 
-    //     let uri =`http://localhost:3000/readOne/${id_aluno}`;
+    //     let uri =`http://localhost:3000/readOne/${id_prof}`;
 
     //     const options = {
     //         method: "POST",
@@ -134,18 +131,26 @@ export default function Perfil({ navigation }) {
                 <Text style={styles.txtEntrar}>Perfil</Text>
                 <Text style={styles.txtSair}>Sair</Text>
             </View>
-            <View style={styles.dados}>
-                    <TouchableOpacity style={styles.turma}>
-                        <Image style={styles.image} source={require('../../../assets/favicon.png')} />
-                        <Text style={styles.titulo}>turminha</Text>
-                        <Image style={styles.image2} source={require('../../../assets/setaBaixo.png')} />
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.turma}>
-                        <Image style={styles.image} source={require('../../../assets/favicon.png')} />
-                        <Text style={styles.titulo}>turminha</Text>
-                        <Image style={styles.image2} source={require('../../../assets/setaBaixo.png')} />
-                    </TouchableOpacity>
-            </View>
+            {
+                info.map((i, index) => {
+                    return (
+                        <View style={styles.dados} key={index}>
+                            <TouchableOpacity style={styles.turma}>
+                                <Image style={styles.image} source={require('../../../assets/favicon.png')} />
+                                <Text style={styles.titulo}>turminha</Text>
+                                <Image style={styles.image2} source={require('../../../assets/setaBaixo.png')} />
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.turma}>
+                                <Image style={styles.image} source={require('../../../assets/favicon.png')} />
+                                <Text style={styles.titulo}>turminha</Text>
+                                <Image style={styles.image2} source={require('../../../assets/setaBaixo.png')} />
+                            </TouchableOpacity>
+                        </View>
+                    )
+                })
+            }
+
+
         </View>
     )
 }
