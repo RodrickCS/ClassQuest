bcdf2b6fba33557076279380b684f922e919bcb8
Andreina Moura <98525153+AndreinaMoura@users.noreply.github.com>
2023-05-22 11:25:34 -0300

    add

--- Mobile/ClassQuest/src/pages/Aluno/perfilAluno/perfilAluno.js
+++ Mobile/ClassQuest/src/pages/Aluno/perfilAluno/perfilAluno.js
@@ -9,12 +9,19 @@ export default function PerfilAluno({ navigation }) {
     const [info, setInfo] = useState([]);
     const [setinhaBaixo, setSetinhaBaixo] = useState(0);
     const [setinhaCima, setSetinhaCima] = useState(0);
+    var dadinhos = info.turma
     const images = [
         require('../../../../assets/setaBaixo.png'),
         require('../../../../assets/setaCima.png'),
     ];
+    const myInterval = setInterval(() => {
+        dados()
+    }, 3000)
 
-    const myInterval = setInterval(() => dados(), 5000)
+    useEffect(() => {
+        dados()
+    }, []);
+
 
     const switchImageNext = () => {
         setSetinhaCima((prevImage) => (prevImage + 1) % images.length)
@@ -34,14 +41,15 @@ export default function PerfilAluno({ navigation }) {
     }
 
     const teste = () => {
-        console.log(info);
+        info.turma.map((dado, index) => {
+            console.log(dado);
+        })
     }
 
     var user = JSON.parse(localStorage.getItem('nome'))
     var id_aluno = (user.id_aluno)
 
     function dados() {
-        // let uri = `` + ;
 
         fetch('http://localhost:3000/alunos/readOne/' + id_aluno)
             .then((resp) => {
@@ -49,27 +57,32 @@ export default function PerfilAluno({ navigation }) {
             })
             .then((data) => {
                 setInfo(data);
+                console.log(data);
             })
     }
-    dados()
     return (
         <View>
             <ImageBackground source={require('../../../../assets/fundo.jpg')} resizeMode="cover" style={styles.imagem}></ImageBackground>
             <View style={styles.divizinha}>
-                <TouchableOpacity onPress={() => { menu() }}>
+                <TouchableOpacity onPress={() => { teste() }}>
                     <Image style={styles.image} source={require('../../../../assets/favicon.png')} />
                 </TouchableOpacity>
                 <Text style={styles.txtEntrar}>Perfil</Text>
                 <Text style={styles.txtSair} onPress={() => { voltar() }}>Sair</Text>
             </View>
-            
-            {/* {
+            {
+                console.log(info.turma)
+            /* {
                 info.map((i, index) => {
                     return (
                         <View style={styles.dados}
-                            key={index}
+                        key={index}
+
                         >
-                            <TouchableOpacity style={styles.turma} onPress={switchImageBefore}>
+                            <TouchableOpacity style={styles.turma}
+                                onPress={switchImageBefore}
+                                // onPress={() => { teste() }}
+                            >
                                 <Image style={styles.image} source={require('../../../../assets/favicon.png')} />
                                 <Text style={styles.titulo}>turminha do aluno</Text>
                                 <Image source={images[setinhaBaixo]} style={styles.image2} />
@@ -80,6 +93,7 @@ export default function PerfilAluno({ navigation }) {
                                 <Image source={images[setinhaCima]} style={styles.image2} />
                             </TouchableOpacity>
                         </View>
+
                     )
                 })
             } */}
