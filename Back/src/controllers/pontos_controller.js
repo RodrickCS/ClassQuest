0ea3223a6cecceb9d0bdc80e31c78ef075ab5acc
RodrickCS <caetanorodrigo46@gmail.com>
2023-06-14 08:11:55 -0300

    a

--- Back/src/controllers/pontos_controller.js
+++ Back/src/controllers/pontos_controller.js
@@ -25,7 +25,7 @@ const create_points_profile = async (req, res) => {
 const add_points = async (req, res) => {
   try {
     let ponto =
-      await prisma.$queryRaw`UPDATE pontos SET qtd = (SELECT qtd FROM pontos WHERE id_aluno = ${req.params.id_aluno} AND id_turma = ${req.params.id_turma} LIMIT 1) + ${req.body.qtd} WHERE id_aluno = ${req.params.id_aluno} AND id_turma = ${req.params.id_turma}`;
+      await prisma.$queryRaw`UPDATE pontos SET qtd = (SELECT qtd WHERE id_aluno = ${req.params.id_aluno} AND id_turma = ${req.params.id_turma} LIMIT 1) + ${req.body.qtd} WHERE id_aluno = ${req.params.id_aluno} AND id_turma = ${req.params.id_turma}`;
     res.status(200).json({ msg: "Pontos atribuídos" }).end();
   } catch (err) {
     res.status(500).json(err).end();
@@ -47,7 +47,7 @@ const removePoints = async (req, res) => {
     });
 
     if (!ponto) {
-      return res.status(404).json({ error: "Ponto não encontrado" }).end();
+      return res.status(404).json({ error: "Profile não encontrado" }).end();
     } else if (ponto[0].qtd < req.body.qtdRemove) {
       return res.status(400).json({ error: "Você não tem pontos suficientes" });
     }
